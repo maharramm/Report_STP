0ee7ac22e775aa58f6e0ac8309d0df5901517ac3
Aliyev Maharram <aliyev.maharramm@mail.ru>
2021-12-07 13:30:36 +0400

    Add project files.

---
+++ ReportSTP/Models/Report/CurrentAccountBalance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportSTP.Models.Report
+{
+    public class CurrentAccountBalance
+    {
+
+        public string Definition { get; set; } 
+        public string Code { get; set; }
+        public double? Remainder { get; set; }
+        public string Currency { get; set; }
+        public string Personnel { get; set; }
+    }
+}
