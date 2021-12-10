2996c4b9b8c07bd2661219cb87634fed2ff09d0d
SenuberAgazade <senuberagazade996@gmail.com>
2021-12-10 13:06:39 +0400

    added user class, modified login controller, view

--- ReportSTP/Controllers/Login/LoginController.cs
+++ ReportSTP/Controllers/Login/LoginController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReportSTP.Models;
+using ReportSTP.Models.Account;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,42 @@ namespace ReportSTP.Controllers.Login
 {
     public class LoginController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public LoginController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginForUser loginForUser)
+        {
+
+            if(loginForUser == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var result = await _context.Users.SingleOrDefaultAsync(u => u.UserName == loginForUser.UserName);
+
+                if (result != null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            
+
+            return View(loginForUser);
+        }
+
+        
     }
 }
---
+++ ReportSTP/Models/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReportSTP.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportSTP.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<LoginForUser>().HasNoKey();
+            builder.Entity<LoginForUser>().ToTable("LoginUser");
+            builder.Entity<LoginForUser>().Property(u => u.UserName).HasColumnName("Username");
+            builder.Entity<LoginForUser>().Property(u => u.Password).HasColumnName("Password");
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<LoginForUser> Users { get; set; }
+
+    }
+}
